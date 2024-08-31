d5237ba64dc3dbefcdaff7eb2395e25e5260073a
gyeom <gyeomii95@naver.com>
2024-08-31 19:20:27 +0900

    Initial Commit

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by 손겸 on 8/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
